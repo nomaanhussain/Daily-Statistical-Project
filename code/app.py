80bb21cd788175b835df2f65d4cc8e5aaaeb7c11
nomaanhussain <nomaanghanchi_11@yahoo.com>
2018-12-29 17:10:14 +0500

    fixed bugs related to date

--- code/app.py
+++ code/app.py
@@ -3,7 +3,7 @@ import sqlite3
 
 app = Flask(__name__)
 
-Fields = ["Date","Inquiry_Call_In","Inquiry_Walk_In","Inquiry_Web","Inquiry_Referral","Inquiry_Other","Total_Inquiries_Warm_Leads","Appointments_Set","Intro_1","Intro_1_Attended_Showed","Intro_2","Intro_2_Attended_Showed","Enrollment_Conferences","Renewal_Upgrade_Conferences","no_of_Cancellations","Dragon","Basic","Mastery","Adults","Total","no_of_Enrollments","Student_Count_1st_Day","Student_Count_2nd_Day","no_of_Upgrades_Renewals","Tuition","Compression","Retail","Special_Event","Promotion_Fee","Down_Payments_Daily","Cancellation_Fees","Other_Revenue","Refunds","Total_Gross_Revenue","Net_Billing_Change","Down_Payments","Monthly_Payment_Total","Down_Payments_Total","Monthly_Payment_Increase","Cancellation_Fee_Collected","Reduction_in_Monthly_Payment"]
+Fields = ["Date","Inquiry_Call_In","Inquiry_Walk_In","Inquiry_Web","Inquiry_Referral","Inquiry_Other","Total_Inquiries_Warm_Leads","Appointments_Set","Intro_1","Intro_1_Attended_Showed","Intro_2","Intro_2_Attended_Showed","Enrollment_Conferences","Renewal_Upgrade_Conferences","no_of_Cancellations","Dragon","Basic","Mastery","Adults","Total","no_of_Enrollments","Student_Count_1st_Day","Student_Count_2nd_Day","no_of_Upgrades_Renewals","Tuition","Compression","Retail","Special_Event","Promotion_Fee","Down_Payments_Daily","Cancellation_Fees","Other_Revenue","Refunds","Total_Gross_Revenue","Net_Billing_Change","Down_Payments","Monthly_Payment_Total","Down_Payments_Total","Monthly_Payment_Increase","Cancellation_Fee_Collected","Reduction_in_Monthly_Payment","no_upgrades_renewals_total","Down_payments_total_total","Monthly_Payments_Increase_total"]
 
 # POST /store_data data
 @app.route('/store_data', methods= ["POST"])
@@ -15,7 +15,7 @@ def insert_record():
         cursor = connection.cursor()
         
         
-        insert_query = "INSERT INTO records VALUES (?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?)"
+        insert_query = "INSERT INTO records VALUES (?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?,?,?,?)"
         # data = (request_data["Date"],request_data["Inquiry_Call_In"],request_data["Inquiry_Walk_In"],request_data["Inquiry_Web"],request_data["Inquiry_Referral"],request_data['Inquiry_Other'],request_data["Total_Inquiries_Warm_Leads"],request_data["Appointments_Set"],request_data["Intro_1"],request_data["Intro_1_Attended_Showed"],request_data["Intro_2"],request_data["Intro_2_Attended_Showed"],request_data["Renewal_Upgrade_Conferences"],request_data["Enrollment_Conferences"],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[],request_data[])
         data =[]
         for k,y in request_data.items():
@@ -47,7 +47,18 @@ def get_store(start_date,end_date):
             single_dict[Fields[index]] = value
         result.append(single_dict)
 
-    return jsonify({'records':result}), 200
+    
+    total= []
+    for field in Fields[1:]:
+        single_dict= {}
+        select_query = "SELECT SUM({}) FROM records where Date>=? AND Date <=?".format(field)
+        for row in cursor.execute(select_query,(start_date,end_date)):
+            single_dict[field] = row
+        total.append(single_dict)
+    return jsonify({'records':result,'total':total}), 200
+
+
+
 
 
 app.run(port=5000)
--- code/create_table.py
+++ code/create_table.py
@@ -15,19 +15,20 @@ create_table = """CREATE TABLE IF NOT EXISTS records (Date date PRIMARY KEY, Inq
  Total int, no_of_Enrollments int, Student_Count_1st_Day int, Student_Count_2nd_Day int, no_of_Upgrades_Renewals int, 
  Tuition real, Compression real, Retail real, Special_Event real, Promotion_Fee real, Down_Payments_Daily real, Cancellation_Fees real, Other_Revenue real, Refunds real,
  Total_Gross_Revenue real, Net_Billing_Change real, Down_Payments real, Monthly_Payment_Total real, Down_Payments_Total real, 
- Monthly_Payment_Increase real, Cancellation_Fee_Collected real, Reduction_in_Monthly_Payment real)"""
+ Monthly_Payment_Increase real, Cancellation_Fee_Collected real, Reduction_in_Monthly_Payment real, no_upgrades_renewals_total int,
+ Down_payments_total_total real, Monthly_Payments_Increase_total real)"""
 cursor.execute(create_table)
 
-# d = "25-06-2003"
+# d = "2003-06-25"
 
-# record = (d, 1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1,1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1)
+# record = (d, 1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1,1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1,1.1,1.1)
 
-# insert_query = "INSERT INTO records VALUES (?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?)"
+# insert_query = "INSERT INTO records VALUES (?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?,?,?,?)"
 
 # cursor.execute(insert_query , record)
 
-# d = "28-06-2003"
-# record = (d, 1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1,1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1)
+# d = "2003-06-28"
+# record = (d, 1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1, 1 , 1,1,1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1.1,1,1.1,1.1)
 # cursor.execute(insert_query , record)
 
 # select_query = "SELECT * FROM records"
@@ -38,6 +39,10 @@ cursor.execute(create_table)
 # select_query = "SELECT * FROM records where Date>=? AND Date <=? "
 
 # for row in cursor.execute(select_query,("25-06-2003","27-06-2003")):
+#     print(row)
+
+# select_query = "SELECT SUM(?) FROM records"
+# for row in cursor.execute(select_query,("Adults",)):
 #     print(row)
 
 connection.commit()
